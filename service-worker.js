545b97407c7995bfe5d80a2e3f6db05a97b436cd
lomogoo <yoshidarikuto@yahoo.com>
2025-06-19 21:51:24 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,17 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
 
+// service-worker.js
 self.addEventListener('install', (e) => {
   console.log('[Service Worker] Installed');
   e.waitUntil(
     caches.open('v1').then((cache) => {
+      // パスを現在のディレクトリ基準に修正
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/app.js',
-        '/manifest.json'
-        // 必要なら他の画像なども追加
+        './',
+        './index.html',
+        './style.css',
+        './app.js',
+        './manifest.json'
       ]);
     })
   );
