cccdc71eac0139122e6436c0794123bb17b5e9e2
lomogoo <yoshidarikuto@yahoo.com>
2025-06-21 12:01:30 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 self.addEventListener('install', (e) => {
   console.log('[Service Worker] Installed');
   e.waitUntil(
-    caches.open('v1').then((cache) => {
+    caches.open('v2').then((cache) => {
       // パスを現在のディレクトリ基準に修正
       return cache.addAll([
         './',
@@ -11,6 +11,20 @@ self.addEventListener('install', (e) => {
         './app.js',
         './manifest.json'
       ]);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== 'v2') {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
